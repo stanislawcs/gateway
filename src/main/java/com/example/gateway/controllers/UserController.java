18876c7e31a154aa8ca4706dec19b88586e6888f
Stanislaw Shukan <shukans588@gmail.com>
2023-12-05 21:11:57 +0300

    Fix controller

--- src/main/java/com/example/gateway/controllers/UserController.java
+++ src/main/java/com/example/gateway/controllers/UserController.java
@@ -6,21 +6,19 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @Slf4j
+@RequestMapping("/users")
 public class UserController {
 
     private final FeignUser feignUser;
 
-    @GetMapping("/users")
+    @GetMapping
     public ResponseEntity<List<ListUserDTO>> getAll(@RequestParam("page") Integer page,
                                                     @RequestParam("users-per-page") Integer usersPerPage) {
         log.info("getAll");
@@ -31,6 +29,5 @@ public class UserController {
     public ResponseEntity<UserDTO> getOneById(@PathVariable("id")Long id){
         return new ResponseEntity<>(feignUser.getOneById(id),HttpStatus.OK);
     }
-
 
 }
