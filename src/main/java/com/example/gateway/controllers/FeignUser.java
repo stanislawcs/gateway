6b0a013fc796488d86624ed365da9d7be907348a
Stanislaw Shukan <shukans588@gmail.com>
2023-12-06 14:17:11 +0300

    add profiles

--- src/main/java/com/example/gateway/controllers/FeignUser.java
+++ src/main/java/com/example/gateway/controllers/FeignUser.java
@@ -4,12 +4,14 @@ import com.example.gateway.controllers.fallback.FeignUserFallback;
 import com.example.gateway.dto.ListUserDTO;
 import com.example.gateway.dto.UserDTO;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
+@Profile("local")
 @FeignClient(value = "users", url = "${users.host}", fallback = FeignUserFallback.class)
 public interface FeignUser {
 
